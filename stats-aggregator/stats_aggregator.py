f3b67f43dc068eed294f05e3f1ddaa373dc54b26
OniOni <mathieu.c.sabourin@gmail.com>
2015-05-09 18:02:46 -0700

    By default only keep a limited number of values in memory.

--- stats-aggregator/stats_aggregator.py
+++ stats-aggregator/stats_aggregator.py
@@ -2,13 +2,18 @@ import statistics
 
 class BaseStatsAggregator(object):
 
-    def __init__(self):
+    def __init__(self, length=500):
         self._stats = {}
         self._supports = []
+        self._max_length = length
 
     def add(self, name, val, **kwargs):
         self._stats[name] = {k: val for k in kwargs if k in self._supports}
         self._stats[name]['vals'] = [val]
+
+    def cleanup(self):
+        if len(self._stats) > self._max_length:
+            self._stats = self._stats[len(self._stats) - self._max_length:]
 
     def update(self, name, val):
         self._stats[name]['vals'].append(val)
@@ -18,6 +23,10 @@ class BaseStatsAggregator(object):
                 continue
 
             self._stats[name][k] = getattr(self, k)(name, val)
+
+        self.cleanup()
+
+        return self.stats()
 
     def stats(self):
         return self._stats
